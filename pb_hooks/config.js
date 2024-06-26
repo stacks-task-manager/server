9bd9c03a0b95b5329614b446c4ff68e3f3620d6a
Synapse <cristian.barlutiu@gmail.com>
2024-06-26 17:00:13 +0200

    updated schema

--- pb_hooks/config.js
+++ pb_hooks/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     cron: {
-        notificationsCheckInterval: "*/3 * * * *", // interval to check for queued emails to be sent
+        transactionalCheckInterval: "*/3 * * * *", // interval to check for queued emails to be sent
         clearLogsInterval: "* * */3 * *", // real-time logs clear interval
+        notificationsInterval: "1 * * * *", // interval to check for queued notification
     }
 };
--- pb_hooks/cron.pb.js
+++ pb_hooks/cron.pb.js
@@ -1,4 +1,12 @@
-cronAdd("logs", require(`${__hooks}/config.js`).cron.notificationsCheckInterval, () => {
+cronAdd("logs", require(`${__hooks}/config.js`).cron.transactionalCheckInterval, () => {
     const utils = require(`${__hooks}/utils.js`);
     utils.clearLogs();
+});
+
+cronAdd("notifications", require(`${__hooks}/config.js`).cron.notificationsInterval, () => {
+    const notifications = arrayOf(new Record);
+
+    $app.dao().recordQuery("notifications")
+        .andWhere($dbx.hashExp({ "sent": "" }))
+        .all(notifications)
 });
